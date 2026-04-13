dbf835e2e2f5a5883bdb08e313a966f1b446d83b
abathur8bit <orion7019@gmail.com>
2026-04-13 08:01:40 -0500

    - version number update that didn't get checked in

--- lib/version.dart
+++ lib/version.dart
@@ -1,6 +1,6 @@
 /// GENERATED FILE - DO NOT EDIT
-const String appVersion = "1.00.00.0002";
+const String appVersion = "1.01.00+0004";
 const int appVersionNumber = 1;
-const int appRevision = 0;
+const int appRevision = 1;
 const int appPatch = 0;
-const int appBuild = 2;
+const int appBuild = 4;
