6512b3f14a6a67bf083de98d37e1f4ae09e36f28
abathur8bit <orion7019@gmail.com>
2026-04-16 11:06:09 -0500

    - Fixing macOS so it builds intel and apple silicon

--- lib/version.dart
+++ lib/version.dart
@@ -1,6 +1,6 @@
 /// GENERATED FILE - DO NOT EDIT
-const String appVersion = "1.1.0+7";
+const String appVersion = "1.1.0+8";
 const int appVersionNumber = 1;
 const int appRevision = 1;
 const int appPatch = 0;
-const int appBuild = 7;
+const int appBuild = 8;
